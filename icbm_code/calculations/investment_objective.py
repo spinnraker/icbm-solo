6e6602141373c627e44d29daaa38295b17860e63
Spinnraker <ecspinnraker@gmail.com>
2021-09-25 06:18:07 -0400

    investment_objective.py = broke down score calculation, created one function per question

--- icbm_code/calculations/investment_objective.py
+++ icbm_code/calculations/investment_objective.py
@@ -1,18 +1,48 @@
 class InvestmentObjective:
     """Calculates the investment objective score"""
 
-    def __init__(self, objective='', objective_score=0):
+    def __init__(self, objective='', objective_final_score=0,
+                 first_answer_score=0, second_answer_score = 0):
         """Defines investment objective attributes"""
         self.objective = objective
-        self.objective_score = objective_score
+        self.objective_final_score = objective_final_score
+        self.first_answer_score = first_answer_score
+        self.second_answer_score = second_answer_score
 
     def get_objective(self):
         """Displays user's investment objective"""
         return self.objective
 
-    def set_objective(self, first_answer, second_answer):
+    # Need two separate functions that calculate first and second answer
+    def calc_first_answer(self, first_answer):
+        """Assigns a value to first investment objective question"""
+        if first_answer == 'a':
+            self.first_answer_score = 1
+        elif first_answer == 'b':
+            self.first_answer_score = 2
+        elif first_answer == 'c':
+            self.first_answer_score = 3
+        elif first_answer == 'd':
+            self.first_answer_score = 4
+        else:
+            print('You entered an invalid value')
+        return self.first_answer_score
+
+    def calc_second_answer(self, second_answer):
+        """Assigns a value to second investment objective question"""
+        if second_answer == 'a':
+            self.second_answer_score = 5
+        elif second_answer == 'b':
+            self.second_answer_score = 3
+        elif second_answer == 'c':
+            self.second_answer_score == 1
+        else:
+            print("You entered an invalid value")
+        return self.second_answer_score
+
+    def set_objective(self, first_answer_score, second_answer_score):
         """Assigns investment objective based on user's input"""
-        final_score = first_answer + second_answer
+        final_score = self.first_answer_score + self.second_answer_score
         if final_score <= 4:
             self.objective = "Income"
         elif (final_score == 5) or (final_score < 7):
@@ -20,8 +50,20 @@ class InvestmentObjective:
         else:
             self.objective = "Growth"
 
-
-inv_obj = InvestmentObjective()
-
-inv_obj.set_objective(1, 1)
-print(inv_obj.get_objective())
+#
+# question_2 = "\nWhen it comes to investing, I am more interested in capital " \
+#              "growth than maintaining the principal value."
+# question_2 += "\na. Strongly Disagree \nb. Somewhat disagree \nc. Neutral" \
+#               "\nd. Somewhat agree \ne.	Strongly agree\nEnter your answer: "
+#
+# answer_2 = input(question_2)
+#
+# question_3 = "\nIf investing in a stock, would you rather:"
+# question_3 += "\na.	Buy Companies that may make significant technological " \
+#               "advances that are still selling at their low initial offering " \
+#               "price. \nb.	Established, well-known companies that have a " \
+#               "potential for continued growth \nc.'Blue chip' stocks that " \
+#               "pay dividends \nEnter your answer: "
+#
+# answer_3 = input(question_3)
+# user_score_io.set_objective(answer_2, answer_3)
--- icbm_code/calculations/main.py
+++ icbm_code/calculations/main.py
@@ -1,10 +1,11 @@
 import time_horizon as th
+import investment_objective as io
 
 # Program asks users a series of questions to determine which
 # securities to suggests (educational purpose ONLY)
 
 # Initialize time horizon object
-user_score = th.TimeHorizon()
+user_score_th = th.TimeHorizon()  # Initialize time horizon instance
 
 # Determine time horizon
 question_1 = "How long are you planning on investing your money?"
@@ -12,15 +13,29 @@ question_1 += "\na. Less than 3 years \nb. 3-5 years\nc. 6-10 years\n" \
               "d. More than 11 years\nEnter your answer: "
 
 answer_1 = input(question_1)
-user_score.set_time(answer_1)
+user_score_th.set_time(answer_1)
 
 # Determine Investment Objective
-question_2 = "When it comes to investing, I am more interested in capital " \
+user_score_io = io.InvestmentObjective()  # Initialize investment objective
+# instance
+
+# Determine time horizon
+question_2 = "\nWhen it comes to investing, I am more interested in capital " \
              "growth than maintaining the principal value."
 question_2 += "\na. Strongly Disagree \nb. Somewhat disagree \nc. Neutral" \
-              "\nd. Somewhat agree \ne.	Strongly agree"
+              "\nd. Somewhat agree \ne.	Strongly agree\nEnter your answer: "
+
 answer_2 = input(question_2)
 
+question_3 = "\nIf investing in a stock, would you rather:"
+question_3 += "\na.	Buy Companies that may make significant technological " \
+              "advances that are still selling at their low initial offering " \
+              "price. \nb.	Established, well-known companies that have a " \
+              "potential for continued growth \nc.'Blue chip' stocks that " \
+              "pay dividends \nEnter your answer: "
 
+answer_3 = input(question_3)
+user_score_io.set_objective(answer_2, answer_3)
 
-print(user_score.get_time())
+print(user_score_th.get_time())
+print(user_score_io.get_objective())
