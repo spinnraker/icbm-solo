e23b9bb92b1779f4622a5dac531bbd623ea1eab1
Spinnraker <ecspinnraker@gmail.com>
2021-09-27 19:55:45 -0400

    added esg category 8

--- icbm_code/tests/test_esg.py
+++ icbm_code/tests/test_esg.py
@@ -109,7 +109,7 @@ class TestESG(unittest.TestCase):
         self.assertIs(self.esg.fourth_answer_score, 5)
 
     def test_esg_set_cat_less_8(self):
-        """Test for scores equal to 4"""
+        """Test for scores less than 8"""
         self.esg.first_answer_score = 1
         self.esg.second_answer_score = 1
         self.esg.third_answer_score = 1
@@ -119,6 +119,15 @@ class TestESG(unittest.TestCase):
         self.assertEqual(self.esg.esg_category, "Low")
         self.assertNotEqual(self.esg.esg_category, "Medium")
         self.assertNotEqual(self.esg.esg_category, "High")
+
+    def test_esg_set_cat_8(self):
+        """Test for scores equal to 8"""
+        self.esg.first_answer_score = 1
+        self.esg.second_answer_score = 1
+        self.esg.third_answer_score = 1
+        self.esg.fourth_answer_score = 5
+        self.esg.set_esg_cat()
+        self.assertIs(self.esg.esg_score, 8)
 
 if __name__ == '__main__':
     unittest.main()
