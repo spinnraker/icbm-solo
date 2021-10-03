a5e5a4e4a58affc1e47884ac1756d0fdb10bd9ca
Spinnraker <ecspinnraker@gmail.com>
2021-10-03 09:35:04 -0400

    updated time horizon unit test

--- icbm_code/tests/test_time_horizon.py
+++ icbm_code/tests/test_time_horizon.py
@@ -12,33 +12,25 @@ class TestTimeHorizon(unittest.TestCase):
         """Test passing value 'a' and returning a th score of 1"""
         self.th.set_time('a')
         self.assertIs(self.th.time_score, 1)
-        self.assertIsNot(self.th.time_score, 3)
-        self.assertEqual(self.th.horizon_category, "Ultra-Short Term")
+        self.assertEqual(self.th.horizon_category, "Short Term")
         self.assertNotEqual(self.th.horizon_category, "Long Term")
+        self.assertNotEqual(self.th.horizon_category, "Intermediate Term")
 
     def test_set_time_b(self):
         """Test passing value 'b' and returning a th score of 2"""
         self.th.set_time('b')
         self.assertIs(self.th.time_score, 2)
-        self.assertIsNot(self.th.time_score, 3)
-        self.assertEqual(self.th.horizon_category, "Short Term")
+        self.assertEqual(self.th.horizon_category, "Intermediate Term")
+        self.assertNotEqual(self.th.horizon_category, "Short Term")
         self.assertNotEqual(self.th.horizon_category, "Long Term")
 
     def test_set_time_c(self):
         """Test passing value 'c' and returning a th score of 3"""
         self.th.set_time('c')
         self.assertIs(self.th.time_score, 3)
-        self.assertIsNot(self.th.time_score, 4)
-        self.assertEqual(self.th.horizon_category, "Intermediate Term")
-        self.assertNotEqual(self.th.horizon_category, "Long Term")
-
-    def test_set_time_d(self):
-        """Test passing value 'd' and returning a th score of 4"""
-        self.th.set_time('d')
-        self.assertIs(self.th.time_score, 4)
-        self.assertIsNot(self.th.time_score, 3)
         self.assertEqual(self.th.horizon_category, "Long Term")
         self.assertNotEqual(self.th.horizon_category, "Intermediate Term")
+        self.assertNotEqual(self.th.horizon_category, "Short Term")
 
 
 if __name__ == '__main__':
