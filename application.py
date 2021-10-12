7cc71fd420afcf935b03160c03195d5cb7e4ab7f
Spinnraker <ecspinnraker@gmail.com>
2021-10-12 15:57:28 -0400

    Completed investment objective forms and routes

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect
-from icbm_code.calculations import time_horizon as th
+from icbm_code.calculations import time_horizon as th, investment_objective as io
 
 user_score_th = th.TimeHorizon()
+user_score_io = io.InvestmentObjective()
 
 app = Flask(__name__)
 
@@ -18,12 +19,26 @@ def start():
     return render_template("time-horizon.html")
 
 
-@app.route("/time-horizon", methods=["get"])
+@app.route("/time-horizon")
 def time_horizon():
     answer = request.args.get("time_horizon")
-    if not answer:
-        return render_template("error.html")
     user_score_th.set_time(answer)
     print(user_score_th.get_time())
-    return render_template("answers.html", answer=request.args.get("time_horizon"))
+    return render_template('/io-first.html')
+    # return render_template("answers.html", answer=request.args.get("time_horizon"))
+
+@app.route("/io-first")
+def io_first():
+    f_answer = request.args.get("io-first")
+    user_score_io.calc_first_answer(f_answer)
+    # return render_template("answers.html", f_answer=request.args.get('io-first'))
+    return render_template("/io-second.html")
+
+@app.route("/io-second")
+def io_second():
+    s_answer = request.args.get("io-second")
+    user_score_io.calc_second_answer(s_answer)
+    user_score_io.set_objective()
+    print(user_score_io.get_objective())
+    return render_template('answers.html', s_answer=request.args.get('io-second'))
 
