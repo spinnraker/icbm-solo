9e45e599071e63f38acc9883b7ce47b84e0152fa
Spinnraker <ecspinnraker@gmail.com>
2021-10-11 20:31:41 -0400

    updated investor_questionnaire.docx

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+# from calculations import time_horizon
 
 app = Flask(__name__)
 
@@ -6,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route("/time-horizon", methods=["GET", "POST"])
+def time_horizon():
+    return render_template("time-horizon.html")
