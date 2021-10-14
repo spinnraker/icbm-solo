62703b5d8cd61a5c6daedc00ca1258d260d591fd
Spinnraker <ecspinnraker@gmail.com>
2021-10-14 05:32:49 -0400

    Left instructions for Nick on application.py

--- application.py
+++ application.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, redirect
-from icbm_code.calculations import time_horizon as th, investment_objective as io
+from icbm_code.calculations import time_horizon as th, \
+    investment_objective as io, risk_profile as rp, esg
 
+# Instantiate classes
 user_score_th = th.TimeHorizon()
 user_score_io = io.InvestmentObjective()
+user_score_rp = rp.RiskProfile()
+user_score_esg = esg.EnvironmentalSocialGovernance()
 
 app = Flask(__name__)
 
+# Each question is on a separate page
 
-@app.route('/')
+@app.route('/')  # What the user sees when visiting the site
 def index():
     return render_template("index.html")
     # return redirect('/start')
@@ -18,6 +23,10 @@ def start():
     # return redirect('/time-horizon')
     return render_template("time-horizon.html")
 
+# How to create new routes or pages:
+
+
+
 
 @app.route("/time-horizon")
 def time_horizon():
@@ -27,6 +36,7 @@ def time_horizon():
     return render_template('/io-first.html')
     # return render_template("answers.html", answer=request.args.get("time_horizon"))
 
+
 @app.route("/io-first")
 def io_first():
     f_answer = request.args.get("io-first")
@@ -34,13 +44,15 @@ def io_first():
     # return render_template("answers.html", f_answer=request.args.get('io-first'))
     return render_template("/io-second.html")
 
-@app.route("/io-second")
-def io_second():
-    s_answer = request.args.get("io-second")
-    user_score_io.calc_second_answer(s_answer)
-    user_score_io.set_objective()
-    print(user_score_io.get_objective())
-    # return render_template('answers.html', s_answer=request.args.get('io-second'))
-    return render_template("/rt-first.html")
 
+
+@app.route("/io-second") # Name of the route will is whatever the last render_template (right above) shows
+def io_second(): # create new function, and named it the same as the route
+    s_answer = request.args.get("io-second") # create variable that will hold the user's answers
+    user_score_io.calc_second_answer(s_answer) # Pass that variable to the function that calculates the score
+    user_score_io.set_objective() # Once you reach the last question of a category, call the SET function
+    print(user_score_io.get_objective())    # After calling SET function, call the GET function
+    return render_template("/rp-first.html") # point to the next question, then go to that HTML page file under templates and create a new form.
+                                            # Repeat the process with as many questions as needed. Once you create all the
+                                            # questions for a category (risk profile, esg, etc.) move to the next category
 
