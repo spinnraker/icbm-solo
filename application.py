241b99eaf112a23fd53a9ca38b5fce5293969ed5
nanders8 <nanders8@villanova.edu>
2021-10-16 10:49:02 -0400

    Update application.py

--- application.py
+++ application.py
@@ -57,6 +57,79 @@ def io_second(): # create new function, and named it the same as the route
     return render_template("/rp-first.html") # point to the next question, then go to that HTML page file under templates and create a new form.
                                             # Repeat the process with as many questions as needed. Once you create all the
                                             # questions for a category (risk profile, esg, etc.) move to the next category
+
+@app.route("/rp.first")
+def rp_first():
+    rp1_answer = request.args.get("rp-first") #var holding user answers
+    user_score_rp.calc_first_answer(rp1_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_rp.set_objective() #SET function
+    print(user_score_rp.get_objective()) #GET function
+    final_answers['Risk Tolerance'] = user_score_rp.get_objective()
+    return render_template("/rp-second.html") #point to next form
+
+@app.route("/rp.second")
+def rp_second():
+    rp2_answer = request.args.get("rp-second") #var holding user answers
+    user_score_rp.calc_second_answer(rp2_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_rp.set_objective() #SET function
+    print(user_score_rp.get_objective()) #GET function
+    final_answers['Risk Tolerance'] = user_score_rp.get_objective()
+    return render_template("/rp-third.html") #point to next form
+
+@app.route("/rp.third")
+def rp_third():
+    rp3_answer = request.args.get("rp-third") #var holding user answers
+    user_score_rp.calc_third_answer(rp3_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_rp.set_objective() #SET function
+    print(user_score_rp.get_objective()) #GET function
+    final_answers['Risk Tolerance'] = user_score_rp.get_objective()
+    return render_template("/rp-fourth.html") #point to next form
+
+@app.route("/rp.fourth")
+def rp_fourth():
+    rp4_answer = request.args.get("rp-fourth") #var holding user answers
+    user_score_rp.calc_fourth_answer(rp4_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_rp.set_objective() #SET function
+    print(user_score_rp.get_objective()) #GET function
+    final_answers['Risk Tolerance'] = user_score_rp.get_objective()
+    return render_template("/esg-first.html") #point to next form
+
+@app.route("/esg.first")
+def esg_first():
+    esg1_answer = request.args.get("esg-first") #var holding user answers
+    user_score_esg.calc_first_answer(esg1_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_esg.set_objective() #SET function
+    print(user_score_esg.get_objective()) #GET function
+    final_answers['ESG'] = user_score_esg.get_objective()
+    return render_template("/esg-second.html") #point to next form
+
+@app.route("/esg.second")
+def esg_second():
+    esg2_answer = request.args.get("esg-second") #var holding user answers
+    user_score_esg.calc_second_answer(esg2_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_esg.set_objective() #SET function
+    print(user_score_esg.get_objective()) #GET function
+    final_answers['ESG'] = user_score_esg.get_objective()
+    return render_template("/esg-third.html") #point to next form
+
+@app.route("/esg.third")
+def esg_third():
+    esg3_answer = request.args.get("esg-third") #var holding user answers
+    user_score_esg.calc_third_answer(esg3_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_esg.set_objective() #SET function
+    print(user_score_esg.get_objective()) #GET function
+    final_answers['ESG'] = user_score_esg.get_objective()
+    return render_template("/esg-fourth.html") #point to next form
+
+@app.route("/esg.fourth")
+def esg_fourth():
+    esg4_answer = request.args.get("esg-fourth") #var holding user answers
+    user_score_esg.calc_fourth_answer(esg4_answer) #pass var to score calculator #NOTE: Not sure this function is called correctly
+    user_score_esg.set_objective() #SET function
+    print(user_score_esg.get_objective()) #GET function
+    final_answers['ESG'] = user_score_esg.get_objective()
+    return render_template("/answers.html") #point to answers.html, since no questions remain
+
 @app.route("/api")
 def show_results():
     return jsonify(final_answers)
