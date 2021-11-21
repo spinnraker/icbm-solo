3133f2e12864f2d37cd622c45e1b1a7b9ba73001
chrono208 <timdrake208@gmail.com>
2021-11-21 16:51:42 -0500

    gui stuff
    gui stuff

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def mix_calculator():
     # NEW
     urls = []
     for result in etfs:
-        urls.append("https://api.twelvedata.com/time_series?apikey=8f91b729c73c4b57b3ceb054ee727a2f&interval=1day&symbol=" + str(result['ticker']) + "&outputsize=1" )
+        urls.append("https://api.twelvedata.com/time_series?apikey=8f91b729c73c4b57b3ceb054ee727a2f&interval=1day&symbol=" + str(result['symbol']) + "&outputsize=1" )
 
 
     print("Final results")
